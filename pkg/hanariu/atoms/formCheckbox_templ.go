e70b2bf8b0901e7f23ec866150641b704d794a33
Michał Borzuchowski <michalekborzuch@gmail.com>
2026-05-18 16:12:42 +0200

    fixed formcheckbox classes

--- pkg/hanariu/atoms/formCheckbox_templ.go
+++ pkg/hanariu/atoms/formCheckbox_templ.go
@@ -40,7 +40,7 @@ func FormCheckbox(props *FormCheckboxBosons) templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<div class=\"formcheckbox-284\">")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<div class=\"formcheckbox\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
