2403fed0ef90c597aa815d5f595bae24517d91ea
Michał Borzuchowski <michalekborzuch@gmail.com>
2026-05-18 16:38:41 +0200

    fixed spinner classes

--- pkg/hanariu/protons/spinner_templ.go
+++ pkg/hanariu/protons/spinner_templ.go
@@ -75,7 +75,7 @@ func Spinner(props *SpinnerBosons) templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 2, "\" class=\"spinner-1067\"></div>")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 2, "\" class=\"spinner\"></div>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
